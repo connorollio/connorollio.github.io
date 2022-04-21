84719765499c5eb32d5e833c9b0c557a1a2d0967
connorollio <102817600+connorollio@users.noreply.github.com>
2022-04-20 22:05:19 -0400

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -225,5 +225,22 @@
       preloader.remove()
     });
   }
+  
+/**
+*Timeline
+*/
+  var inputs = $(".input");
+  var paras = $(".description-flex-container").find("p");
+  inputs.click(function () {
+	var t = $(this),
+		ind = t.index(),
+		matchedPara = paras.eq(ind);
+
+	t.add(matchedPara).addClass("active");
+	inputs.not(t).add(paras.not(matchedPara)).removeClass("active");
+});
 
 })()
+
+
+
